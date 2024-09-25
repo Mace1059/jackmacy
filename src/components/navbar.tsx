b6390f877b1b184281cb562af1a7f01b98c62ecd
Jack Macy <jackmacy123@gmail.com>
2024-09-25 02:23:35 -0400

    updated resume

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -14,7 +14,7 @@ function NavBar() {
         <a href="/about">
           About
         </a>
-        <a href={"https://mui.com/material-ui/getting-started/installation/"} target="_blank">
+        <a href={"https://drive.google.com/file/d/11g59plnHMsgZxKLeOyKArwZ8mRUMLPIP/view?usp=sharing"} target="_blank">
           Resume
         </a>
         <a href={"https://www.linkedin.com/in/jack-macy-b227a8239/"} target="_blank">
